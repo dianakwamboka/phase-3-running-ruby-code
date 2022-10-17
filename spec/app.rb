19f2547bac48c44fb090bcf2dffebb277402cbb0
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-10-17 12:34:27 +0300

    completed assignment

---
+++ spec/app.rb
@@ -0,0 +1,21 @@
+# app.rb
+# Put and Print are Great for working with strings
+puts "Hello World!"
+
+
+print "Pass this test, please."
+print "Hello  world!"
+print "Hello  world!"
+
+
+#p for non-strings objects & pp (Pretty printing) for complex or nested arrays and hashes 
+puts [1, 2, 3]
+p [1, 2, 3]
+
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+#EXPLORING WITH RUBY IRB(Interactive Ruby) used for running a ruby REPL(ReadEvaluatePrintLoop)
+#Puts method always returns nil
+
+#RSpec a gem that provides  domain speciofic language(DSL)
+
